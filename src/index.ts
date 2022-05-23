d9accff705b6c3d55179c32d3345868662ffae22
Korin <386303472@qq.com>
2022-05-23 21:29:57 +0800

    press Enter keyboard button to start the game

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,15 @@ import './style/index.less';
 import GameControls from './modules/GameControls';
 
 
-const gameControls = new GameControls(); // 创建游戏实例来开始和进行游戏
+const gameControls = new GameControls(); // 创建gameControls实例来开始和进行游戏
 
 // 点击开始按钮开始游戏
-gameControls.startEle.addEventListener('click', async function () {
-  gameControls.initGame(); // 每次开始游戏前都要初始化下数据
+gameControls.startEle.addEventListener('click', function () {
+  gameControls.initGame(); // 每次开始游戏前都要初始化下数据（开始游戏）
 });
 
+window.addEventListener('keydown', function (e) {
+  if (e.key === 'Enter') {
+    gameControls.initGame(); // 按下回车键初始化下数据（开始游戏）
+  }
+})
